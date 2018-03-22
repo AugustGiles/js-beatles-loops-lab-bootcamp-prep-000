5cc9218b00bf4ce6b1daf5f058b2b77987497a6e
Taylor August Giles <augustgiles8@gmail.com>
2018-03-22 02:26:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function beatlesLoops(array1, array2) {
   var newArray = []; 
-  for (n=0; n < array1.length; n++) {
-    newArray.push(`${array1[n]} plays ${array2[n]})
-  }
+  for (n = 0; n < array1.length; n++) {
+    newArray.push(`${array1[n]} plays ${array2[n]}`);
+  } return newArray;
 }
+
